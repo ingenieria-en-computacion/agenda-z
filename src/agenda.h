464a86a18b94991da4736e5a8798e5abe2fba806
lunalaviada-png <luna.laviada@gmail.com>
2025-11-15 10:09:56 -0600

    Update agenda.h

--- src/agenda.h
+++ src/agenda.h
@@ -1,4 +1,4 @@
-#ifndef __AGENDA_H__
+/*#ifndef __AGENDA_H__
 #define __AGENDA_H__
 
 #define MAX_CONTACTOS 100
@@ -38,4 +38,52 @@ void ordenar_personas(Agenda *agenda);
 void ordenar_personas_inv(Agenda *agenda);
 void menu(Agenda a);
 
+#endif*/
+
+#ifndef __AGENDA_H__
+#define __AGENDA_H__
+
+#define MAX_CONTACTOS 100
+
+// Enum para tipo de teléfono
+typedef enum { CASA, MOVIL, OFICINA, OTRO } TipoTelefono;
+
+// Enum para tipo de contacto
+typedef enum { AMIGO, CONOCIDO, LABORAL } TipoContacto;
+
+// Enum para mes de nacimiento
+typedef enum { ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO,
+               AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE } Mes;
+
+// Estructura de contacto
+typedef struct {
+    char nombre[30];
+    char apellido[30];
+    int diaNacimiento;
+    Mes mesNacimiento;
+    char telefono[20];
+    TipoTelefono tipoTelefono;
+    TipoContacto tipoContacto;
+} Contacto;
+
+// Estructura de agenda
+typedef struct {
+    Contacto contactos[MAX_CONTACTOS];
+    int num_contactos;
+} Agenda;
+
+// Funciones
+void iniciar_agenda(Agenda *agenda);
+void agregar_contacto(Agenda *agenda, Contacto c);
+int buscar_contacto(Agenda *agenda, char *nombre);
+int buscar_contacto_x_telefono(Agenda *agenda, char telefono[]);
+void ordenar_contactos(Agenda *agenda);
+void ordenar_contactos_inv(Agenda *agenda);
+void mostrar_contacto(Contacto c);
+void leer_contacto(Contacto *c);
+void imprimir_agenda(Agenda agenda);
+void cargar_contactos(char *filename, Agenda *agenda);
+void guardar_contactos(char *filename, Agenda agenda);
+
 #endif
+
