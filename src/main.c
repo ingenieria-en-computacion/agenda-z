c1595eb9584c88478dd96d6ca7678daed3306e9d
lunalaviada-png <luna.laviada@gmail.com>
2025-11-15 10:11:46 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,18 +1,64 @@
 #include <stdio.h>
-#include <string.h>
 #include "agenda.h"
 
-int main() {
-    Agenda a;
+int main(){
+    Agenda agenda;
+    iniciar_agenda(&agenda);
 
-    strcpy(a.directorio, "contactos");
-    sprintf(a.rutaArchivo, "%s/contactos.txt", a.directorio);
+    char archivo[] = "contactos.txt";
+    cargar_contactos(archivo,&agenda);
 
-    iniciar_agenda(&a);
-    crearDirectorio(a);
-    crearTXT(a);
-    cargar_contactos(&a);
-    menu(a);
+    int opcion;
+    do{
+        printf("\n=== MENU ===\n");
+        printf("1. Agregar persona\n");
+        printf("2. Mostrar agenda\n");
+        printf("3. Buscar persona\n");
+        printf("4. Ordenar A-Z\n");
+        printf("5. Ordenar Z-A\n");
+        printf("6. Guardar contactos\n");
+        printf("0. Salir\n");
+        printf("Elige una opcion: "); scanf("%d",&opcion);
+
+        switch(opcion){
+            case 1:{
+                Contacto c;
+                leer_contacto(&c);
+                agregar_contacto(&agenda,c);
+                break;
+            }
+            case 2:
+                imprimir_agenda(agenda);
+                break;
+            case 3:{
+                char nombre[30];
+                printf("Nombre a buscar: "); scanf("%s",nombre);
+                int idx = buscar_contacto(&agenda,nombre);
+                if(idx>=0) mostrar_contacto(agenda.contactos[idx]);
+                else printf("No encontrado.\n");
+                break;
+            }
+            case 4:
+                ordenar_contactos(&agenda);
+                printf("Contactos ordenados A-Z.\n");
+                break;
+            case 5:
+                ordenar_contactos_inv(&agenda);
+                printf("Contactos ordenados Z-A.\n");
+                break;
+            case 6:
+                guardar_contactos(archivo,agenda);
+                printf("Contactos guardados.\n");
+                break;
+            case 0:
+                guardar_contactos(archivo,agenda);
+                printf("Saliendo...\n");
+                break;
+            default:
+                printf("Opcion invalida.\n");
+        }
+
+    }while(opcion!=0);
 
     return 0;
 }
