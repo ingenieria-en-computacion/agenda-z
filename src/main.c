ec63fdc83a9fae00dfd485c5e5e1095164ec9298
lunalaviada-png <luna.laviada@gmail.com>
2025-11-15 09:52:19 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-#include <locale.h>
 #include "agenda.h"
 
 int main() {
-
     Agenda a;
 
     strcpy(a.directorio, "contactos");
     sprintf(a.rutaArchivo, "%s/contactos.txt", a.directorio);
-    a.max_contactos = 50;
 
+    iniciar_agenda(&a);
     crearDirectorio(a);
     crearTXT(a);
+    cargar_contactos(&a);
     menu(a);
 
     return 0;
 }
+
+
